5750cd9d7e804e064793c5ff29e42ee5a7fa7d9e
LunaOak <lariosa1@yahoo.com>
2017-04-20 12:51:23 -0400

    commit

--- app/src/main/java/edu/umd/cs/jobi/CompanyActivity.java
+++ app/src/main/java/edu/umd/cs/jobi/CompanyActivity.java
@@ -1,7 +1,12 @@
 package edu.umd.cs.jobi;
 
 
+import android.support.v4.app.Fragment;
 
-public class CompanyActivity {
+public class CompanyActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+       return CompanyFragment.newInstance();
+    }
 
 }
