987ac7d9244193def1a35cb2180e8ff8356ea2e8
pchan1 <pchan1@umd.edu>
2017-05-06 14:37:20 -0400

    Working out db

---
+++ app/src/main/java/edu/umd/cs/jobi/service/impl/JobiPositionContactDbHelper.java
@@ -0,0 +1,35 @@
+package edu.umd.cs.jobi.service.impl;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Pauline on 5/5/2017.
+ */
+
+public class JobiPositionContactDbHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "jobi_position_contact.db";
+    private static final int VERSION = 1;
+
+    public JobiPositionContactDbHelper(Context context) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + JobiPositionDbSchema.ContactTable.NAME + "(" +
+                " _id integer primary key autoincrement, " +
+                JobiPositionDbSchema.ContactTable.Columns.ID + ", " +
+                JobiPositionDbSchema.ContactTable.Columns.JOB_TITLE + ", " +
+                JobiPositionDbSchema.ContactTable.Columns.NAME + ", " +
+                JobiPositionDbSchema.ContactTable.Columns.EMAIL + ", " +
+                JobiPositionDbSchema.ContactTable.Columns.PHONE +
+                ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
