b9715db2d480225a649fcbe46501df590a6481d9
nicolebtorres <nicolebrtorres@gmail.com>
2017-05-06 11:28:36 -0400

    set up other private members

--- app/src/main/java/edu/umd/cs/jobi/EventListFragment.java
+++ app/src/main/java/edu/umd/cs/jobi/EventListFragment.java
@@ -8,9 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import edu.umd.cs.jobi.model.Event;
+import edu.umd.cs.jobi.service.EventService;
 
 public class EventListFragment extends Fragment {
 
+    private EventService eventService;
+    private List<Event> allEvents;
     private RecyclerView eventList;
 
     public static EventListFragment newInstance() {
@@ -22,6 +28,9 @@ public class EventListFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        eventService = DependencyFactory.getEventService(getActivity().getApplicationContext());
+        allEvents = eventService.getAllEvents();
     }
 
     @Nullable
