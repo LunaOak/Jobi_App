67d22c2b405ccd2b43a2b38e070aa1a877201b8c
pchan1 <pchan1@umd.edu>
2017-05-07 20:49:51 -0400

    Delete position in positionList

--- app/src/main/java/edu/umd/cs/jobi/service/PositionService.java
+++ app/src/main/java/edu/umd/cs/jobi/service/PositionService.java
@@ -8,6 +8,7 @@ import edu.umd.cs.jobi.model.Position;
 
 public interface PositionService {
     public void addPositionToDb(Position position);
+    public boolean deletePositionById(String id);
     public Position getPositionById(String id);
     public List<Position> getAllPositions();
     public List<Position> getFavoritePositions();
