39f9761f55ba4ff6d4e24124bcf42b7206bd04ac
jlalonso123 <jlalonso123@gmail.com>
2017-05-08 01:18:31 -0400

    Replaced address, city, and state with location

--- app/src/main/java/edu/umd/cs/jobi/model/Event.java
+++ app/src/main/java/edu/umd/cs/jobi/model/Event.java
@@ -14,9 +14,7 @@ public class Event implements Serializable {
     private String position;
     private Type type;
     private Date date;
-    private String address;
-    private String city;
-    private String state;
+    private String location;
     private List<Contact> contacts;
     private List<Reminder> reminders;
 
@@ -105,31 +103,11 @@ public class Event implements Serializable {
     }
 
     public String getLocation() {
-        return address + ", " + city + ", " + state;
+        return location;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     public List<Contact> getContacts() {
