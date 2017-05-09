c7d5beedcc6b0cf1eac4dd1490a11b5ee0f24d8f
Anwesha Naskar <anweshanaskar@gmail.com>
2017-05-09 14:09:42 -0400

    Fixed settings & settings database

--- app/src/main/java/edu/umd/cs/jobi/HomeFragment.java
+++ app/src/main/java/edu/umd/cs/jobi/HomeFragment.java
@@ -88,14 +88,8 @@ public class HomeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
         companyService = DependencyFactory.getCompanyService(getActivity().getApplicationContext());
-        //settings = DependencyFactory.getSettingsService(getActivity().getApplicationContext()).getSettings();
-        if (savedInstanceState !=null) {
-            String settingsId = getArguments().getString(SETTINGS_ID);
-            //settingsService =  DependencyFactory.getSettingsService(getActivity().getApplicationContext());
-            settings = DependencyFactory.getSettingsService(getActivity().getApplicationContext()).getSettings(settingsId);
-        } else {
-            settingsService = DependencyFactory.getSettingsService(getActivity().getApplicationContext());
-        }
+        settingsService = DependencyFactory.getSettingsService(getActivity().getApplicationContext());
+        settings = settingsService.getSettings("1");
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
