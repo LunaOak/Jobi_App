b5f081e57ad34fd36046bd23aea69c62dd797f8a
nicolebtorres <nicolebrtorres@gmail.com>
2017-05-09 03:02:33 -0400

    cancelling edit position

--- app/src/main/java/edu/umd/cs/jobi/PositionFragment.java
+++ app/src/main/java/edu/umd/cs/jobi/PositionFragment.java
@@ -1,5 +1,6 @@
 package edu.umd.cs.jobi;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -209,6 +210,9 @@ public class PositionFragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_CANCELED) {
+            return;
+        }
         if (requestCode == REQUEST_CODE_EDIT_POSITION) {
             position = EnterPositionActivity.getPositionCreated(data);
             positionService.addPositionToDb(position);
